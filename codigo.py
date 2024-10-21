1320d7565176b9879fbd74c878b39cdf5d433f9f
AmosRibeiro <amosribeiro2708@gmail.com>
2024-10-21 19:23:40 -0300

    variaveis em python

--- codigo.py
+++ codigo.py
@@ -1,1 +1,20 @@
 print("Olá, mundo!")
+
+faturamento = 1000
+custo = 700
+
+imposto = faturamento * 0.1
+
+lucro = faturamento - custo - imposto
+
+margem_lucro = lucro / faturamento
+
+print("O faturamento foi de", faturamento)
+print("O custo foi de", custo)
+print("O lucro foi de", lucro)
+print("A margem de lucro foi de", round(margem_lucro, 1))
+
+mensagem = "O faturamento da loja foi legal"
+email = "email@email.com"
+
+teve_lucro = True
